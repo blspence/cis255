075c32caa42d69da1efb4a6f5b0c7bad079ff67a
blspence <blspence@svsu.edu>
2018-05-26 20:48:08 -0400

    html validation and form stub

---
+++ archive/courses.php
@@ -0,0 +1,110 @@
+<html>
+
+<head>
+    <link rel="icon" href="img/cardinal_logo.png" type="image/png" />
+</head>
+
+<body>
+<h1 align="center">SVSU/CSIS Department</h1>
+<h2 align="center"></h2>
+
+<h3>Spring</h3>
+    <table border='3' width='100%'>
+        <tr bgcolor='yellow'>
+            <td width='13%'>CIS 255*70</td>
+            <td width='40%'>Client Side Web Appl Devel (0050)</td>
+            <td width='12%'>Avl:12 (30)</td>
+            <td width='15%'>T 12:30 PM</td>
+            <td width='10%'>SE 135</td>
+            <td width='10%'>gpcorser</td>
+        </tr>
+        <tr bgcolor='lightblue'>
+            <td width='13%'>CIS 357*11</td>
+            <td width='40%'>Advanced Programming With Java (0052)</td>
+            <td width='12%'>Avl:15 (30)</td>
+            <td width='15%'>MW 12:30 PM</td>
+            <td width='10%'>SE 117</td>
+            <td width='10%'>icho</td>
+        </tr>
+    </table>
+    <br/>
+    <table border='3' width='100%'>
+        <tr bgcolor='lightblue'>
+            <td width='13%'>CS 146*11</td>
+            <td width='40%'>Visual Basic.NET Programming (0074)</td>
+            <td width='12%'>Avl:27 (30)</td>
+            <td width='15%'>TR 12:30 PM</td>
+            <td width='10%'>SE 145</td>
+            <td width='10%'>icho</td>
+        </tr>
+        <tr bgcolor='#B19CD9'>
+            <td width='13%'>CS 345*70</td>
+            <td width='40%'>UNIX & System Administration (0076)</td>
+            <td width='12%'>Avl:4 (30)</td>
+            <td width='15%'>T 08:00 AM</td>
+            <td width='10%'>SE 124</td>
+            <td width='10%'>james</td>
+        </tr>
+    </table>
+    <br/>
+
+No data available<br/>
+
+<h3>Summer</h3>
+    <table border='3' width='100%'>
+        <tr bgcolor='yellow'>
+            <td width='13%'>CIS 355*70</td>
+            <td width='40%'>Server Side Web App Dev (3032)</td>
+            <td width='12%'>Avl:10 (30)</td>
+            <td width='15%'>M 08:00 AM</td>
+            <td width='10%'>SE 135</td>
+            <td width='10%'>gpcorser</td>
+        </tr>
+    </table>
+    <br/>
+    <table border='3' width='100%'>
+        <tr bgcolor='#FFB347'>
+            <td width='13%'>CS 105*90</td>
+            <td width='40%'>Intro to Computers & Programmg (3044)</td>
+            <td width='12%'>Avl:26 (30)</td>
+            <td width='15%'> </td>
+            <td width='10%'>(Online)</td>
+            <td width='10%'>amulahuw</td>
+        </tr>
+        <tr bgcolor='#FFB347'>
+            <td width='13%'>CS 160*90</td>
+            <td width='40%'>Microcomputer Software (3046)</td>
+            <td width='12%'>Avl:21 (25)</td>
+            <td width='15%'> </td> <td width='10%'>(Online)</td>
+            <td width='10%'>amulahuw</td>
+        </tr>
+        <tr bgcolor='pink'>
+            <td width='13%'>CS 245*31</td>
+            <td width='40%'>Stat & Its Appl in Comp Sci (3048)</td>
+            <td width='12%'>Avl:10 (30)</td>
+            <td width='15%'>MW 12:30 PM</td>
+            <td width='10%'>SE 135</td>
+            <td width='10%'>krahman</td>
+        </tr>
+    </table>
+    <br/>
+    <table border='3' width='100%'>
+        <tr bgcolor='pink'>
+            <td width='13%'>CSIS 582*31</td>
+            <td width='40%'>AI and Expert Systems (3050)</td>
+            <td width='12%'>Avl:20 (30)</td>
+            <td width='15%'>MW 07:00 PM</td>
+            <td width='10%'>SE 119</td>
+            <td width='10%'>krahman</td>
+        </tr>
+    </table>
+    <br/>
+
+<h3>Fall</h3>
+<table border='3' width='100%'><tr bgcolor='yellow'><td width='13%'>CIS 255*70</td><td width='40%'>Client Side Web Appl Devel (0422)</td><td width='12%'>Avl:0 (30)</td><td width='15%'>M 12:30 PM</td><td width='10%'>SE 135</td><td width='10%'>gpcorser</td></tr><tr bgcolor='yellow'><td width='13%'>CIS 255*71</td><td width='40%'>Client Side Web Appl Devel (0424)</td><td width='12%'>Avl:12 (30)</td><td width='15%'>W 12:30 PM</td><td width='10%'>SE 135</td><td width='10%'>gpcorser</td></tr><tr bgcolor='#B19CD9'><td width='13%'>CIS 333*01</td><td width='40%'>Intro to System Software (0426)</td><td width='12%'>Avl:0 (25)</td><td width='15%'>MW 10:30 AM</td><td width='10%'>SE 119</td><td width='10%'>james</td></tr><tr bgcolor='lightblue'><td width='13%'>CIS 386*01</td><td width='40%'>Database Management Systems (0428)</td><td width='12%'>Avl:13 (30)</td><td width='15%'>MW 02:30 PM</td><td width='10%'>SW 143</td><td width='10%'>icho</td></tr><tr bgcolor='#B19CD9'><td width='13%'>CIS 422*01</td><td width='40%'>System Analy & Design Concepts (0430)</td><td width='12%'>Avl:4 (30)</td><td width='15%'>TR 08:30 AM</td><td width='10%'>SE 121</td><td width='10%'>james</td></tr><tr bgcolor='#B19CD9'><td width='13%'>CIS 425*01</td><td width='40%'>Decision Support Systems (0432)</td><td width='12%'>Avl:0 (24)</td><td width='15%'>MW 12:30 PM</td><td width='10%'>SE 116</td><td width='10%'>james</td></tr></table><br/><table border='3' width='100%'><tr><td width='13%'>CS 105*01</td><td width='40%'>Intro to Computers & Programmg (0624)</td><td width='12%'>Avl:6 (27)</td><td width='15%'>T 10:30 AM</td><td width='10%'>SE 117</td><td width='10%'>staff1</td></tr><tr><td width='13%'>CS 105*02</td><td width='40%'>Intro to Computers & Programmg (0626)</td><td width='12%'>Avl:5 (27)</td><td width='15%'>MW 10:30 AM</td><td width='10%'>SE 145</td><td width='10%'>ryerion</td></tr><tr><td width='13%'>CS 116*01</td><td width='40%'>Computer Programming I (0630)</td><td width='12%'>Avl:4 (28)</td><td width='15%'>MW 10:30 AM</td><td width='10%'>SE 135</td><td width='10%'>staff1</td></tr><tr bgcolor='#77DD77'><td width='13%'>CS 116*02</td><td width='40%'>Computer Programming I (0632)</td><td width='12%'>Avl:13 (28)</td><td width='15%'>TR 08:30 AM</td><td width='10%'>SE 145</td><td width='10%'>pdharam</td></tr><tr><td width='13%'>CS 146*01</td><td width='40%'>Visual Basic.NET Programming (0634)</td><td width='12%'>Avl:24 (27)</td><td width='15%'>M 07:00 PM</td><td width='10%'>SE 145</td><td width='10%'>rbeyers</td></tr><tr><td width='13%'>CS 146*02</td><td width='40%'>Visual Basic.NET Programming (0636)</td><td width='12%'>Avl:15 (27)</td><td width='15%'>MW 12:30 PM</td><td width='10%'>SE 145</td><td width='10%'>nas</td></tr><tr><td width='13%'>CS 146*03</td><td width='40%'>Visual Basic.NET Programming (0638)</td><td width='12%'>Avl:14 (27)</td><td width='15%'>TR 04:30 PM</td><td width='10%'>SE 145</td><td width='10%'>rbeyers</td></tr><tr><td width='13%'>CS 150*01</td><td width='40%'>Contemporary Business Software (0640)</td><td width='12%'>Avl:0 (28)</td><td width='15%'>W 09:30 AM</td><td width='10%'>SE 145</td><td width='10%'>ryerion</td></tr><tr><td width='13%'>CS 150*02</td><td width='40%'>Contemporary Business Software (0642)</td><td width='12%'>Avl:0 (28)</td><td width='15%'>W 08:30 AM</td><td width='10%'>SE 145</td><td width='10%'>ryerion</td></tr><tr><td width='13%'>CS 160*90</td><td width='40%'>Microcomputer Software (0644)</td><td width='12%'>Avl:8 (30)</td><td width='15%'> </td> <td width='10%'>(Online)</td><td width='10%'>eschenk</td></tr><tr><td width='13%'>CS 160*91</td><td width='40%'>Microcomputer Software (0646)</td><td width='12%'>Avl:29 (30)</td><td width='15%'> </td> <td width='10%'>(Online)</td><td width='10%'>eschenk</td></tr><tr bgcolor='#FFB347'><td width='13%'>CS 216*01</td><td width='40%'>Computer Programming II (0648)</td><td width='12%'>Avl:12 (30)</td><td width='15%'>MW 08:30 AM</td><td width='10%'>SE 135</td><td width='10%'>amulahuw</td></tr><tr bgcolor='#77DD77'><td width='13%'>CS 216*02</td><td width='40%'>Computer Programming II (0650)</td><td width='12%'>Avl:13 (30)</td><td width='15%'>MW 02:30 PM</td><td width='10%'>SE 145</td><td width='10%'>pdharam</td></tr><tr bgcolor='yellow'><td width='13%'>CS 232*90</td><td width='40%'>Cybersecurity Sys Admin (0652)</td><td width='12%'>Avl:0 (25)</td><td width='15%'> </td> <td width='10%'>(Online)</td><td width='10%'>gpcorser</td></tr><tr bgcolor='pink'><td width='13%'>CS 245*01</td><td width='40%'>Stat & Its Appl in Comp Sci (0654)</td><td width='12%'>Avl:0 (30)</td><td width='15%'>T 10:30 AM</td><td width='10%'>SE 145</td><td width='10%'>krahman</td></tr><tr bgcolor='pink'><td width='13%'>CS 316*01</td><td width='40%'>Data Struc & Algorithm Analy (0656)</td><td width='12%'>Avl:4 (30)</td><td width='15%'>MW 12:30 PM</td><td width='10%'>P 242</td><td width='10%'>krahman</td></tr><tr bgcolor='pink'><td width='13%'>CS 333*01</td><td width='40%'>Computer Forensics I (0658)</td><td width='12%'>Avl:17 (30)</td><td width='15%'>MW 08:30 AM</td><td width='10%'>SE 224</td><td width='10%'>krahman</td></tr><tr bgcolor='#77DD77'><td width='13%'>CS 401*01</td><td width='40%'>Computer Networks (0660)</td><td width='12%'>Avl:0 (30)</td><td width='15%'>MW 10:30 AM</td><td width='10%'>SE 120</td><td width='10%'>pdharam</td></tr><tr bgcolor='yellow'><td width='13%'>CS 403*90</td><td width='40%'>Mobile App Dev (0662)</td><td width='12%'>Avl:13 (20)</td><td width='15%'> </td> <td width='10%'>(Online)</td><td width='10%'>gpcorser</td></tr><tr bgcolor='lightblue'><td width='13%'>CS 421*01</td><td width='40%'>Object-Oriented Des & Develop (0664)</td><td width='12%'>Avl:7 (30)</td><td width='15%'>TR 12:30 PM</td><td width='10%'>SE 117</td><td width='10%'>icho</td></tr><tr bgcolor='#FFB347'><td width='13%'>CS 461*01</td><td width='40%'>Theory of Computation (0666)</td><td width='12%'>Avl:0 (30)</td><td width='15%'>TR 10:30 AM</td><td width='10%'>SE 225</td><td width='10%'>amulahuw</td></tr></table><br/><table border='3' width='100%'><tr bgcolor='yellow'><td width='13%'>CSIS 503*90</td><td width='40%'>Mobile Computing (0668)</td><td width='12%'>Avl:25 (30)</td><td width='15%'> </td> <td width='10%'>(Online)</td><td width='10%'>gpcorser</td></tr><tr><td width='13%'>CSIS 521*01</td><td width='40%'>Software Engineering (0670)</td><td width='12%'>Avl:22 (30)</td><td width='15%'>MW 05:30 PM</td><td width='10%'>SE 117</td><td width='10%'>staff1</td></tr><tr bgcolor='#FFB347'><td width='13%'>CSIS 531*01</td><td width='40%'>Computer Architecture (0672)</td><td width='12%'>Avl:25 (30)</td><td width='15%'>TR 05:30 PM</td><td width='10%'>SE 117</td><td width='10%'>amulahuw</td></tr><tr><td width='13%'>CSIS 566*01</td><td width='40%'>IT Strategy (0674)</td><td width='12%'>Avl:21 (30)</td><td width='15%'>M 07:00 PM</td><td width='10%'>SE 121</td><td width='10%'>mdstackh</td></tr></table><br/><h3>Winter</h3><table border='3' width='100%'><tr bgcolor='#77DD77'><td width='13%'>CIS 301*01</td><td width='40%'>Data Comm & Network Admin (0420)</td><td width='12%'>Avl:3 (30)</td><td width='15%'>TR 10:30 AM</td><td width='10%'>SE 145</td><td width='10%'>pdharam</td></tr><tr bgcolor='#B19CD9'><td width='13%'>CIS 311*01</td><td width='40%'>Windows Prog. with VB.NET (0422)</td><td width='12%'>Avl:0 (30)</td><td width='15%'>MW 12:30 PM</td><td width='10%'>SE 223</td><td width='10%'>james</td></tr><tr bgcolor='yellow'><td width='13%'>CIS 355*01</td><td width='40%'>Server Side Web App Dev (0424)</td><td width='12%'>Avl:0 (30)</td><td width='15%'>MW 10:30 AM</td><td width='10%'>SE 145</td><td width='10%'>gpcorser</td></tr><tr bgcolor='#77DD77'><td width='13%'>CIS 357*01</td><td width='40%'>Advanced Programming With Java (0426)</td><td width='12%'>Avl:0 (30)</td><td width='15%'>TR 12:30 PM</td><td width='10%'>SE 221</td><td width='10%'>pdharam</td></tr><tr bgcolor='#B19CD9'><td width='13%'>CIS 366*01</td><td width='40%'>IT Project Management (0428)</td><td width='12%'>Avl:11 (25)</td><td width='15%'>MW 08:30 AM</td><td width='10%'>SE 116</td><td width='10%'>james</td></tr><tr bgcolor='#B19CD9'><td width='13%'>CIS 390*01</td><td width='40%'>Special Topics (0430)</td><td width='12%'>Avl:0 (1)</td><td width='15%'> </td> <td width='10%'>TBA TBA</td><td width='10%'>james</td></tr><tr bgcolor='#B19CD9'><td width='13%'>CIS 424*01</td><td width='40%'>System Det Design, Impl & Mgmt (0432)</td><td width='12%'>Avl:6 (30)</td><td width='15%'>TR 08:30 AM</td><td width='10%'>SE 125</td><td width='10%'>james</td></tr></table><br/><table border='3' width='100%'><tr><td width='13%'>CS 105*01</td><td width='40%'>Intro to Computers & Programmg (0606)</td><td width='12%'>Avl:18 (27)</td><td width='15%'>MW 02:30 PM</td><td width='10%'>SE 135</td><td width='10%'>staff1</td></tr><tr><td width='13%'>CS 116*01</td><td width='40%'>Computer Programming I (0610)</td><td width='12%'>Avl:20 (29)</td><td width='15%'>TR 08:30 AM</td><td width='10%'>SE 145</td><td width='10%'>staff1</td></tr><tr bgcolor='yellow'><td width='13%'>CS 116*02</td><td width='40%'>Computer Programming I (0612)</td><td width='12%'>Avl:16 (29)</td><td width='15%'>MW 12:30 PM</td><td width='10%'>SE 145</td><td width='10%'>gpcorser</td></tr><tr><td width='13%'>CS 146*01</td><td width='40%'>Visual Basic.NET Programming (0614)</td><td width='12%'>Avl:22 (27)</td><td width='15%'>TR 04:30 PM</td><td width='10%'>SE 145</td><td width='10%'>rbeyers</td></tr><tr><td width='13%'>CS 146*02</td><td width='40%'>Visual Basic.NET Programming (0616)</td><td width='12%'>Avl:24 (27)</td><td width='15%'>MW 08:30 AM</td><td width='10%'>SE 145</td><td width='10%'>nas</td></tr><tr><td width='13%'>CS 150*01</td><td width='40%'>Contemporary Business Software (0618)</td><td width='12%'>Avl:15 (28)</td><td width='15%'>T 12:30 PM</td><td width='10%'>SE 145</td><td width='10%'>staff1</td></tr><tr><td width='13%'>CS 150*02</td><td width='40%'>Contemporary Business Software (0620)</td><td width='12%'>Avl:16 (28)</td><td width='15%'>T 01:30 PM</td><td width='10%'>SE 145</td><td width='10%'>staff1</td></tr><tr><td width='13%'>CS 160*01</td><td width='40%'>Microcomputer Software (0622)</td><td width='12%'>Avl:25 (30)</td><td width='15%'>MW 08:30 AM</td><td width='10%'>P 232</td><td width='10%'>ryerion</td></tr><tr><td width='13%'>CS 160*91</td><td width='40%'>Microcomputer Software (0624)</td><td width='12%'>Avl:24 (30)</td><td width='15%'> </td> <td width='10%'>(Online)</td><td width='10%'>eschenk</td></tr><tr bgcolor='pink'><td width='13%'>CS 216*01</td><td width='40%'>Computer Programming II (0626)</td><td width='12%'>Avl:1 (30)</td><td width='15%'>MW 10:30 AM</td><td width='10%'>SE 135</td><td width='10%'>krahman</td></tr><tr bgcolor='yellow'><td width='13%'>CS 233*01</td><td width='40%'>Cybercrime (0628)</td><td width='12%'>Avl:10 (25)</td><td width='15%'>TR 08:30 AM</td><td width='10%'>SE 137</td><td width='10%'>gpcorser</td></tr><tr bgcolor='#FFB347'><td width='13%'>CS 331*01</td><td width='40%'>Computer Org & Assembly Lang (0630)</td><td width='12%'>Avl:1 (30)</td><td width='15%'>MW 08:30 AM</td><td width='10%'>SE 123</td><td width='10%'>amulahuw</td></tr><tr><td width='13%'>CS 411*01</td><td width='40%'>Database Systems (0632)</td><td width='12%'>Avl:5 (30)</td><td width='15%'>MW 02:30 PM</td><td width='10%'>SE 117</td><td width='10%'>staff1</td></tr><tr bgcolor='pink'><td width='13%'>CS 433*01</td><td width='40%'>Cybersecurity (0634)</td><td width='12%'>Avl:22 (30)</td><td width='15%'>TR 12:30 PM</td><td width='10%'>P 245</td><td width='10%'>krahman</td></tr><tr bgcolor='#FFB347'><td width='13%'>CS 446*01</td><td width='40%'>Operating Systems (0636)</td><td width='12%'>Avl:0 (30)</td><td width='15%'>TR 10:30 AM</td><td width='10%'>SE 224</td><td width='10%'>amulahuw</td></tr><tr><td width='13%'>CS 451*01</td><td width='40%'>Prog Lang Concepts (0638)</td><td width='12%'>Avl:0 (30)</td><td width='15%'>MW 08:30 AM</td><td width='10%'>SE 119</td><td width='10%'>staff1</td></tr><tr bgcolor='lightblue'><td width='13%'>CS 471*01</td><td width='40%'>Senior CS Capstone (0640)</td><td width='12%'>Avl:7 (30)</td><td width='15%'>MW 12:30 PM</td><td width='10%'>SE 124</td><td width='10%'>icho</td></tr></table><br/><table border='3' width='100%'><tr bgcolor='#77DD77'><td width='13%'>CSIS 501*01</td><td width='40%'>Computer Network & Security (0642)</td><td width='12%'>Avl:29 (30)</td><td width='15%'>MW 05:30 PM</td><td width='10%'>SE 125</td><td width='10%'>pdharam</td></tr><tr><td width='13%'>CSIS 586*01</td><td width='40%'>Cloud & Big Data Analytics (0644)</td><td width='12%'>Avl:28 (30)</td><td width='15%'>M 07:00 PM</td><td width='10%'>SE 126</td><td width='10%'>kjhillak</td></tr><tr bgcolor='lightblue'><td width='13%'>CSIS 621*01</td><td width='40%'>Software Architect & Design (0646)</td><td width='12%'>Avl:27 (30)</td><td width='15%'>T 07:00 PM</td><td width='10%'>SE 117</td><td width='10%'>icho</td></tr><tr bgcolor='pink'><td width='13%'>CSIS 633*70</td><td width='40%'>Inform Security and Privacy (0648)</td><td width='12%'>Avl:27 (30)</td><td width='15%'>T 05:30 PM</td> <td width='10%'>SE 117</td><td width='10%'>krahman</td></tr></table><br/><a href="coursesearch.php" class="btn btn-primary">Search</a></body></html>
+
+
+
+
+
+
